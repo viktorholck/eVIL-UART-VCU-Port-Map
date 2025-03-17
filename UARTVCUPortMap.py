649e82f35a971614ffe118cf8137068576dd5227
viktorholck <vholck@volvocars.com>
2025-03-17 13:18:57 +0100

    Add initial project files including icon, configuration, and main scripts

---
+++ UARTVCUPortMap.py
@@ -0,0 +1,106 @@
+"""
+This script VCU UART tagets to their corresponding COM port address.
+Classes:
+    VCUPort: A data class representing various ports in a VCU system and their corresponding COM port addresses.
+Functions:
+    map_vcu_ports(ports: serial.tools.list_ports_common.ListPortInfo) -> VCUPort:
+    main():
+"""
+import argparse
+import json
+import platform
+import re
+import sys
+from dataclasses import asdict, dataclass
+from typing import Optional
+import serial
+
+# If running on Windows patch the serial.tools.list_ports to the list_ports_windows local copy
+# To fix a known issue with the serial library on Windows not showing all information for FTDI devices
+if platform.system() == "Windows":
+    import list_ports_windows_patched_from_pyserial_3_5
+    serial.tools.list_ports = list_ports_windows_patched_from_pyserial_3_5  # Patch the serial.tools.list_ports module to enable identificatcon of serial ports on the FTDI serial hub
+
+
+@dataclass
+class VCUPort:
+    """
+    VCUPort class represents various ports in a VCU (Vehicle Control Unit) system. And the corresponding COM port addresses.
+
+    Attributes:
+        HPA (Optional[str])
+        HIA (Optional[str])
+        HIB (Optional[str])
+        LPA (Optional[str])
+        SGA (Optional[str])
+    """
+    HPA: Optional[str] = None
+    HIA: Optional[str] = None
+    HIB: Optional[str] = None
+    LPA: Optional[str] = None
+    SGA: Optional[str] = None
+
+
+def map_vcu_ports(ports: serial.tools.list_ports_common.ListPortInfo) -> VCUPort:
+    """
+    Maps the VCU ports to their corresponding COM port addresses.
+
+    Args:
+        ports (serial.tools.list_ports_common.ListPortInfo): List of serial ports.
+
+    Returns:
+        VCUPort: An instance of VCUPort with mapped ports.
+    """
+    port_map = VCUPort()
+    for port in ports:
+        if port.location == 1 or re.match(r".*:1\.0", str(port.location)):
+            port_map.HIA = port.device
+        elif port.location == 2 or re.match(r".*:1\.1", str(port.location)):
+            port_map.HIB = port.device
+        elif port.location == 3 or re.match(r".*:1\.2", str(port.location)):
+            port_map.HPA = port.device
+        elif port.location == 4 or re.match(r".*:1\.3", str(port.location)):
+            port_map.LPA = port.device
+        elif port.location == 5 or re.match(r".*:1\.4", str(port.location)):  # Not valid/available on verC UART board's
+            port_map.SGA = port.device
+    return port_map
+
+
+def main():
+    """
+    Main function to list all serial ports, filter valid ones for uart_control, and print the port map in JSON format.
+    """
+    argparser = argparse.ArgumentParser(description="List all serial ports and filter valid ones for uart_control. Select a port by serial number or COM port.")
+    argparser.add_argument("--debug", "-d", action="store_true", help="Enable debug mode")
+    args = argparser.parse_args()
+
+    ports = serial.tools.list_ports.comports()
+    valid_ports = []  # list of valid ports
+    invalid_ports = []  # list of invalid ports
+
+    for port in sorted(ports):
+        if port.vid == 1027 and port.pid == 24593:
+            valid_ports.append(port)
+        else:
+            invalid_ports.append(port)
+
+    if args.debug:
+        print("\n################# Invalid Ports:")
+        for port in invalid_ports:
+            print(f'Port.__dict__: {port.__dict__}\n')
+
+        print("\n################# Valid Ports:")
+        for port in valid_ports:
+            print(f'Port.__dict__: {port.__dict__}\n')
+
+        print("\n################# Port map:")
+
+    json_output = json.dumps(asdict(map_vcu_ports(valid_ports)), indent=4)
+    print(json_output)
+    if not valid_ports:
+        sys.exit(1)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ generateExecutable.py
@@ -0,0 +1,124 @@
+"""
+This module provides functionality to generate an executable from a Python script using PyInstaller.
+It supports building the executable either locally or within a development container.
+Functions:
+- run_pyinstaller(script: str, workpath: str): Runs PyInstaller to create a one-file bundled executable.
+- print_output(label: str, output: subprocess.CompletedProcess): Prints the attributes of a subprocess.CompletedProcess object.
+- build_executable_in_devcontainer(): Builds a (Linux) executable in a development container.
+Usage:
+- Run the script without any flags to build both locally & in a development container.
+- Run the script with the --container flag to build and run in a development container.
+- Run the script with the --local flag to build and run locally.
+- FileNotFoundError: If the 'devcontainer' command is not available in the PATH.
+- ValueError: If the containerId cannot be retrieved from the devcontainer output.
+- Exception: If the platform is unsupported.
+"""
+import argparse
+import json
+import os
+import platform
+import shutil
+import subprocess
+import sys
+
+import PyInstaller.__main__
+
+
+def run_pyinstaller(script: str, workpath: str):
+    """
+    Run PyInstaller to create a one-file bundled executable.
+
+    :param script: The script to be bundled into an executable.
+    :param workpath: The directory to use for the build process.
+    """
+    PyInstaller.__main__.run([
+        script,
+        '--onefile',  # Create a one-file bundled executable
+        '--workpath', workpath,  # The directory to use for the build process
+        '--icon', 'team-evil.ico',
+        '--clean'  # Clean the build directory before building, makes the build slower but more reliable
+    ])
+
+
+def print_output(label: str, output: subprocess.CompletedProcess):
+    """
+    Print the attributes of a subprocess.CompletedProcess object.
+
+    :param label: A label to identify the output.
+    :param output: The subprocess.CompletedProcess object to print.
+    """
+    print(f'{label}:')
+    for attr in dir(output):
+        if not callable(getattr(output, attr)) and not attr.startswith('_'):
+            print(f'{attr}: {getattr(output, attr)}')
+    print('#################################################################')
+
+
+def build_executable_in_devcontainer():
+    """
+    Build a (linux) executable in a development container.
+    """
+    try:
+        # Check if the devcontainer command is available
+        devcontainer_path = shutil.which('devcontainer')
+        if devcontainer_path is None:
+            raise FileNotFoundError("The 'devcontainer' command is not available in your PATH.")
+
+        # Build the development container
+        # buildResult = subprocess.run(['devcontainer', 'build', '--workspace-folder', '.'], shell=True, capture_output=True, text=True, check=True)
+        # printOutput("Container build", buildResult)
+
+        # Run the development container
+        up_result = subprocess.run(['devcontainer', 'up', '--workspace-folder', '.'], shell=True, capture_output=True, text=True, check=True, encoding='utf-8')
+        print_output("Container up", up_result)
+
+        # Parse the JSON output to get the containerId
+        container_info = json.loads(up_result.stdout)
+        container_id = container_info.get("containerId")
+        # remoteWorkspaceFolder = containerInfo.get("remoteWorkspaceFolder")
+        if not container_id:
+            raise ValueError("Failed to get containerId from the output")
+
+        # Run the script to generate the executable
+        exec_result = subprocess.run(['devcontainer', 'exec', '--container-id', container_id, '--workspace-folder', '.', 'python', f'{os.path.basename(__file__)}', '--local'], shell=True, capture_output=True, text=True, check=True, encoding='utf-8')
+        print_output("Script execution", exec_result)
+
+        # Stop and delete the container
+        # downResult = subprocess.run(['devcontainer', 'down', '--container-id', containerId], shell=True, capture_output=True, text=True, check=True)  # Not working in cli yet
+        # print(f'downResult: {downResult}')
+
+        # Stop and remove the container using Docker commands
+        stop_result = subprocess.run(['docker', 'stop', container_id], shell=True, capture_output=True, text=True, check=True, encoding='utf-8')
+        print_output("Container stop", stop_result)
+
+        remove_result = subprocess.run(['docker', 'rm', container_id], shell=True, capture_output=True, text=True, check=True)
+        print_output("Container remove", remove_result)
+
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+        print("Make sure the 'devcontainer' command is available in your PATH.")
+        sys.exit(1)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description="Generate executable for UARTVCUPortMap")
+    argBuildGroup = argparser.add_mutually_exclusive_group()
+    argBuildGroup.add_argument("--container", help="Build and run in devcontainer", action="store_true")
+    argBuildGroup.add_argument("--local", help="Build and run locally", action="store_true")
+    args = argparser.parse_args()
+
+    if not args.local:
+        build_executable_in_devcontainer()
+
+    if not args.container:
+        if platform.system() == "Windows":
+            WORKPATH = 'buildWindows'
+        elif platform.system() == "Linux":
+            WORKPATH = 'buildLinux'
+        else:
+            raise Exception('Unsupported platform')
+
+        run_pyinstaller('UARTVCUPortMap.py', WORKPATH)
